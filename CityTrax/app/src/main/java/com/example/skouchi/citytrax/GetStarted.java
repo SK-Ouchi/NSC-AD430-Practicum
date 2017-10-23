bcbce6d65f7b893bce5bd3919f7ec0e692f99050
kazerwit <kazerwit@users.noreply.github.com>
2017-10-22 23:13:13 -0700

    Add onclicklistener for start button

--- CityTrax/app/src/main/java/com/example/skouchi/citytrax/GetStarted.java
+++ CityTrax/app/src/main/java/com/example/skouchi/citytrax/GetStarted.java
@@ -1,13 +1,35 @@
 package com.example.skouchi.citytrax;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class GetStarted extends AppCompatActivity {
+public class GetStarted extends AppCompatActivity implements View.OnClickListener {
+
+    private Button startButton;
+    private static final String TAG = "GetStarted";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started);
+
+        startButton = (Button) findViewById(R.id.buttonStart);
+        startButton.setOnClickListener(GetStarted.this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        try {
+            Intent intent = new Intent(this, Details.class);
+            startActivity(intent);
+        } catch (Exception e) {
+            Log.d(TAG, "Start Button Clicked");
+        }
     }
 }
