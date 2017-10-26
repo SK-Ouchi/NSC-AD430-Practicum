86ec7b60474330f11c1282c398cce293da04fdf8
Samuel Cooledge <weconnect@Samuels-MacBook-Pro.local>
2017-10-26 09:10:43 -0700

    Added click listener/FAB to volunteer creation page

--- CityTrax/app/src/main/java/com/example/skouchi/citytrax/VolunteerCreationPage.java
+++ CityTrax/app/src/main/java/com/example/skouchi/citytrax/VolunteerCreationPage.java
@@ -1,7 +1,10 @@
 package com.example.skouchi.citytrax;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.view.View;
 
 public class VolunteerCreationPage extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class VolunteerCreationPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_volunteer_creation_page);
+
+
+        //FAB Save
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabSave);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        //FAB Home
+        FloatingActionButton fab2 = (FloatingActionButton) findViewById(R.id.fabHome);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
+
 }
